07a5f5b2d8bf1ed927008bf523fb6cee9dd30e36
stil <p@ryciuk.com>
2016-08-14 00:35:23 +0200

    Add email and password required in tests .env

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,6 @@
 <?php
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+$dotenv->required(['ERPK_EMAIL', 'ERPK_PASSWORD']);
